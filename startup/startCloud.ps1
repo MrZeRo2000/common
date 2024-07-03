fd39514b7ca02210f820712f3683cbeb84fb5530
MrZeRo2000 <MrZeRo2000@mail.ru>
2024-07-03 23:19:20 +0300

    addd start cloud script

---
+++ startup/startCloud.ps1
@@ -0,0 +1,19 @@
+
+
+Write-Host "Starting" -ForegroundColor DarkGray
+
+do {
+  Write-Host "Checking volume ..." -ForegroundColor DarkMagenta
+  $volumeFound = Test-Path "R:\"
+
+  if (!($volumeFound)) {
+    Start-Sleep -Seconds 1
+  }
+
+} until ($volumeFound)
+Write-Host "Volume found" -ForegroundColor DarkGreen
+
+$startPath = "$env:LOCALAPPDATA\Microsoft\OneDrive\OneDrive.exe"
+Write-Host "Starting $startPath ..." -ForegroundColor DarkMagenta
+Start-Process -FilePath $startPath -ArgumentList "/background"
+Write-Host "Started" -ForegroundColor DarkGreen

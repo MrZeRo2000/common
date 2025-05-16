916561e50158e75ad8eafbc8f93d005877c3f2bc
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-05-16 18:07:13 +0300

    added template for calculation of sizes

--- backup/compareFolderSize.ps1
+++ backup/compareFolderSize.ps1
@@ -14,3 +14,19 @@ $folders | ForEach-Object {
     }
 }
 
+foreach ($folder in $folders) {
+    # $folderSize = (Get-ChildItem -Path $folder -Recurse | Measure-Object -Property Length -Sum).Sum
+    # Write-Host "Folder: $folder, Size: $($folderSize / 1MB) MB" -ForegroundColor DarkGray
+}
+
+<#
+
+Get-ChildItem -Path ../data/f1 -Recurse -File | Group-Object -Property Directory | ForEach-Object {
+    @{
+        DirectoryName = $_.Name
+        FileCount     = $_.Group.Count
+        TotalSize   = (($_.Group | Measure-Object -Property Length -Sum).Sum)
+    }
+}
+
+#>

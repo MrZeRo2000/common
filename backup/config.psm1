8fff27a665e860e7cb85cd309d086b98a2ffed4c
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-05-23 14:40:48 +0300

    Fix logic for archive root path

--- backup/config.psm1
+++ backup/config.psm1
@@ -12,13 +12,15 @@ Function Get-BackupConfig {
 		throw "WinRAR not found in the expected locations"
 	}
 
-	$targetRootPaths = "R:/OneDrive/", "$env:USERPROFILE\OneDrive\"
+    $targetRootPath = $env:ONEDRIVE
 
-	$targetRootPath = $targetRootPaths | ForEach-Object -Process {
-		if (Test-Path -Path $_) {
-			return $_      
-		}
-	}
+    if ($null -eq $targetRootPath) {
+        $targetRootPath = "R:/OneDrive/", "$env:USERPROFILE\OneDrive\" | ForEach-Object -Process {
+            if (Test-Path -Path $_) {
+                return $_      
+            }
+        }
+    }
 
 	if ($null -eq $targetRootPath) {
 		throw "Target root path not found"

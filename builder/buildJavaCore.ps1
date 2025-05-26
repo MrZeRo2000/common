de8055ad4238fc30c11259ce7a9559b36d0160be
MrZeRo2000 <MrZeRo2020@outlook.com>
2025-05-26 15:30:01 +0300

    Small adjustment to builder

--- builder/buildJavaCore.ps1
+++ builder/buildJavaCore.ps1
@@ -2,7 +2,6 @@ $ErrorActionPreference = "Stop"
 
 Import-Module $PSScriptRoot\builder.psm1
 
-
 if (Set-Java-Home) {
   Build -projects 'jutils-core violetnote-core' -command 'clean publishToMavenLocal'
 }
